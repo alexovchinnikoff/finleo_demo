41417e7fd0f302d23863c6de7b70161c03d7d584
Aleksandr Ovchinnikov <alexander.ovchinnikoff@gmail.com>
2026-08-07 18:42:34 +0300

    Update test_utils.py

--- test_utils.py
+++ test_utils.py
@@ -1,9 +1,8 @@
-# tests_demo/test_utils.py
 import pytest
 from utils import get_even_numbers
 
 class TestGetEvenNumbers:
-    """Тестирование функции фильтрации чётных чисел."""
+    """Тестирование функции фильтрации четных чисел."""
 
     def test_mixed_numbers(self):
         """Тест со смешанным списком."""
@@ -16,17 +15,17 @@ class TestGetEvenNumbers:
         assert get_even_numbers([]) == []
 
     def test_no_even_numbers(self):
-        """Тест, когда чётных чисел нет."""
+        """Тест, когда четных чисел нет."""
         data = [1, 3, 5]
         assert get_even_numbers(data) == []
 
     def test_negative_and_zero(self):
-        """Тест с отрицательными числами и нулём (0 — чётное)."""
+        """Тест с отрицательными числами и нулем (0 — четное)."""
         data = [-3, -2, -1, 0, 1, 2]
         expected = [-2, 0, 2]
         assert get_even_numbers(data) == expected
 
     def test_all_even(self):
-        """Тест, когда все числа чётные."""
+        """Тест, когда все числа четные."""
         data = [2, 4, 8, 10]
         assert get_even_numbers(data) == data 
